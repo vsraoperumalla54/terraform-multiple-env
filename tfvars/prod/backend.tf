e6efedb79be1c79ce230655f0673d57fc699d124
venkata.perumalla <venkata.perumalla@pragmaticplay.com>
2025-11-02 23:57:01 +0530

    terraform

---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket = "terraform-86-daws-prod"
+key    = "tfvars-multi-env-demo"
+region = "us-east-1"
+use_lockfile = true
+encrypt = true
---
+++ tfvars/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+    backend "s3" {
+    bucket = "terraform-86-daws"
+    key    = "provisioners-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
