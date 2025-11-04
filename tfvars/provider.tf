8efa9642e36bfbdff448a2c2573223d89890106e
venkata.perumalla <venkata.perumalla@pragmaticplay.com>
2025-11-05 00:46:29 +0530

    terraform

--- tfvars/provider.tf
+++ tfvars/provider.tf
@@ -6,11 +6,7 @@ terraform {
     }
   }
     backend "s3" {
-    bucket = "terraform-86-daws"
-    key    = "provisioners-demo"
-    region = "us-east-1"
-    use_lockfile = true
-    encrypt = true
+    
     }
 }
 
